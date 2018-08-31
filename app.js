c066652f157ac2cd041e80326a40c76b898fe809
Joel Scheuner <joel.scheuner.dev@gmail.com>
2018-08-31 13:16:48 +0200

    Add empty camels storage array

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
+// Camels storage array
+var camels = [];
+
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
 });
